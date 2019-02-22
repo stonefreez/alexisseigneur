9f5ff1140f9eff46d136fb1948770b0406e96811
stonefreez <36804186+stonefreez@users.noreply.github.com>
2019-02-22 18:43:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NTQxMDU5MjQ3MzA0MDgxNDA5.D1HFUA.qObAUPvhQCeIZ7RJ8ssnkXfixII';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = 'm!Baise lana';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyBStU5eJgdHZkM3ngsQKyMurWvgf8dUTbs';
